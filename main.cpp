927bb3397bc3d7e46fb03b32ec6b832970be782e
Rushikesh8421 <patilrushikesh8421@gmail.com>
2021-04-27 17:57:23 +0530

    Starting with c++

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+
+// int main()
+// {
+    // std::cout<<"Hello World\n";
+    // return 0;
+// }
+
+// OR
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum = 3, to;
+    cout<<"Hello Rushikesh! "<<sum<<"\n";
+    cin>>to;
+    cout<<"The formated output is "<<to<<" and the sum is "<<sum<<"\n";
+    return 0;
+}
