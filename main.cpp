426fb4512b070737b8400d16c2da02744c798a92
Rushikesh8421 <patilrushikesh8421@gmail.com>
2021-04-27 18:07:37 +0530

    Accessing global and local variable in cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@
 
 #include<iostream>
 using namespace std;
+
+int G = 1002;
+
 int main()
 {
-    int sum = 3, to;
+    int sum = 3, to, G = 2001;
     cout<<"Hello Rushikesh! "<<sum<<"\n";
     cin>>to;
     cout<<"The formated output is "<<to<<" and the sum is "<<sum<<"\n";
+    // how to access for global variable and local variable in cpp 
+    cout<<"The local variable is: "<<G<<" and the global variable is: "<<::G<<endl;
+    cout<<"Ended!"<<endl;
     return 0;
 }
